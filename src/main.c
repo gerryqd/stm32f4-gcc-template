72d2447f661f2e838b95c5c561c12b29c814b899
Gerry WANG <wangfengjn@gmail.com>
2017-02-21 20:04:04 +0800

    Add 'delay()' function to make LED blink

--- src/main.c
+++ src/main.c
@@ -1,14 +1,20 @@
 #include <stm32f4xx_conf.h>
 
+void delay(void)
+{
+  for (volatile unsigned int i=0; i<10000000; i++)
+    ;
+}
 
 int main (void)
 {
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
 
-	GPIOD->MODER = (1 << 26);
+  GPIOD->MODER = (1 << 26);
 
-	while (1)
-	{
-		GPIOD->ODR ^= (1 << 13);
-	}
+  while (1)
+  {
+    GPIOD->ODR ^= (1 << 13);
+    delay();
+  }
 }
