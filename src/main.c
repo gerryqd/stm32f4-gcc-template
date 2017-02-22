e15cfad77529c1e2f1208e45686b6d097083e652
Gerry WANG <wangfengjn@gmail.com>
2017-02-22 19:00:14 +0800

    User button example
    If the user button is pressed, the led will be lighten,
    otherwise, it will be off.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
+#include <stdbool.h>
+#include <stdint.h>
+
 #include <stm32f4xx_conf.h>
 
 #define LED_PIN   GPIO_Pin_14
+
+#define USER_BUTTON_PIN   GPIO_Pin_0
 
 static void led_init(void)
 {
@@ -13,6 +18,23 @@ static void led_init(void)
   GPIO_Init(GPIOD, &gpio);
 }
 
+static void button_init(void)
+{
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+  GPIO_InitTypeDef gpio;
+  GPIO_StructInit(&gpio);
+  gpio.GPIO_Mode = GPIO_Mode_IN;
+  gpio.GPIO_Pin = USER_BUTTON_PIN;
+  GPIO_Init(GPIOA, &gpio);
+}
+
+static bool is_button_pressed(void)
+{
+  uint8_t pin_status = GPIO_ReadInputDataBit(GPIOA, USER_BUTTON_PIN);
+  return pin_status == SET;
+}
+
 static void led_on(void)
 {
   GPIO_SetBits(GPIOD, LED_PIN);
@@ -23,22 +45,21 @@ static void led_off(void)
   GPIO_ResetBits(GPIOD, LED_PIN);
 }
 
-static void delay(void)
-{
-  for (volatile unsigned int i=0; i<10000000; i++)
-    ;
-}
-
 int main(void)
 {
   led_init();
+  button_init();
 
   while(1)
   {
-    led_on();
-    delay();
-    led_off();
-    delay();
+    if (is_button_pressed())
+    {
+      led_on();
+    }
+    else
+    {
+      led_off();
+    }
   }
 }
 
